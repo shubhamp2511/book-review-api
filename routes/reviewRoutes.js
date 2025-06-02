547e3bbe934b608a5ca5357db008a8d65eab410a
shubhamp2511 <shubham.darshi2511@gmail.com>
2025-06-02 21:25:43 +0530

    Added routes folder
    Added authRoutes , bookRoutes and reviewRoutes file as API endpoints

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/authMiddleware');
+const { addReview, updateReview, deleteReview } = require('../controllers/reviewController');
+
+router.post('/books/:id/reviews', auth, addReview);
+router.put('/reviews/:id', auth, updateReview);
+router.delete('/reviews/:id', auth, deleteReview);
+module.exports = router;
